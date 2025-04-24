46802fdefb3fb6aa5b12903ed925ea31d3a54a65
Bram-Simon <55248906+Bram-Simon@users.noreply.github.com>
2025-04-24 10:05:12 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MOSNA'
+copyright = 'year, name_author'
+author = 'name_author'
 
 release = '0.1'
 version = '0.1.0'
